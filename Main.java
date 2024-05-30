1ad9cf545ca94b8c688fc2ee3e702c3bba009a3a
Ameya <559514@cuhsd.org>
2024-05-30 13:58:59 -0700

    sorry for being mean GitHub

--- Main.java
+++ Main.java
@@ -9,138 +9,130 @@ public class Main {
 
     public static void main(String args[]) throws IOException {
         //System.out.println("This is my final project!");
-        Scanner input = new Scanner(System.in);
+        boolean done = false;
+        while(!done) {
+            Scanner input = new Scanner(System.in);
 
-        //ArrayList<Book> read = new ArrayList<Book>();
-        //ArrayList<Book> tbr = new ArrayList<Book>();
-        ArrayList<Double> read = new ArrayList<Double>();
-        //int[] topTen = new int[10];
+            ArrayList<Double> read = new ArrayList<Double>();
 
-        int userChoice = 0;
-        String userAction = "idk";
+            int userChoice = 0;
+            String userAction = "idk";
 
-        while(userChoice != 1 && userChoice != 2) {
-            System.out.println("What would you like to do? \nAccess your lists(1) \nAdd to a list(2)");
-            userChoice = input.nextInt();
-
-            if (userChoice == 1) {
-                System.out.println("Which list would you like to access? \nTBR(1) \nRead(2)");
-                userChoice = input.nextInt();
-                if (userChoice == 1){
-                    userAction = "accessTBR";
-                }
-                else if (userChoice == 2){
-                    userAction = "accessRead";
-                }
-                else{
-                    System.out.println("That wasn't an option, you should try again");
-                }
-
-            }
-            else if (userChoice == 2) {
-                System.out.println("Which list would you like to add to? \nTBR(1) \nRead(2)");
+            while (userChoice != 1 && userChoice != 2) {
+                System.out.println("\nWhat would you like to do? \nAccess your lists(1) \nAdd to a list(2) \nQuit(3)");
                 userChoice = input.nextInt();
 
-                if (userChoice == 1){
-                    userAction = "modifyTBR";
+                if (userChoice == 1) {
+                    System.out.println("Which list would you like to access? \nTBR(1) \nRead(2)");
+                    userChoice = input.nextInt();
+                    if (userChoice == 1) {
+                        userAction = "accessTBR";
+                    } else if (userChoice == 2) {
+                        userAction = "accessRead";
+                    } else {
+                        System.out.println("That wasn't an option, you should try again");
+                    }
+
+                } else if (userChoice == 2) {
+                    System.out.println("Which list would you like to add to? \nTBR(1) \nRead(2)");
+                    userChoice = input.nextInt();
+
+                    if (userChoice == 1) {
+                        userAction = "modifyTBR";
+                    } else if (userChoice == 2) {
+                        userAction = "modifyRead";
+                    } else {
+                        System.out.println("That wasn't an option, you should try again");
+                    }
+
                 }
-                else if (userChoice == 2){
-                    userAction = "modifyRead";
+                else if(userChoice == 3){
+                    done = true;
+                    break;
                 }
-                else{
+                if (userChoice != 1 && userChoice != 2 && userChoice != 3) {
                     System.out.println("That wasn't an option, you should try again");
                 }
-
             }
-            if(userChoice != 1 && userChoice != 2){
-                System.out.println("That wasn't an option, you should try again");
-            }
-        }
 
-        //System.out.println(userChoice);
-        input.nextLine();
+            input.nextLine();
 
-        if(userAction.equals("modifyTBR")){
-            System.out.println("Title:");
-            String title = input.nextLine();
+            if (userAction.equals("modifyTBR")) {
+                System.out.println("Title:");
+                String title = input.nextLine();
 
-            System.out.println("Type:");
-            String type = input.nextLine();
+                System.out.println("Type:");
+                String type = input.nextLine();
 
-            System.out.println("Trope:");
-            String trope = input.nextLine();
+                System.out.println("Trope:");
+                String trope = input.nextLine();
 
-            System.out.println("Author:");
-            String author = input.nextLine();
+                System.out.println("Author:");
+                String author = input.nextLine();
 
-            System.out.println("Pages:");
-            int numPages = input.nextInt();
+                System.out.println("Pages:");
+                int numPages = input.nextInt();
 
-            Book myBook;
+                Book myBook;
 
-            if(!trope.isEmpty()){
+                if(trope.equals("")){
+                    myBook = new Fiction(title, author, type, trope, numPages);
+                }
+                else{
+                    myBook = new Book(title, author, type, numPages);
+                }
+
+                FileWriter fWriter = new FileWriter("C:\\Users\\Ameya\\IdeaProjects\\LibraryProject\\tbr.txt", true);
+                fWriter.write(String.valueOf(myBook) + "\n");
+                fWriter.close();
+
+            } else if (userAction.equals("modifyRead")) {
+                System.out.println("Title:");
+                String title = input.nextLine();
+
+                System.out.println("Type:");
+                String type = input.nextLine();
+
+                System.out.println("Trope:");
+                String trope = input.nextLine();
+
+                System.out.println("Author:");
+                String author = input.nextLine();
+
+                System.out.println("Pages:");
+                int numPages = input.nextInt();
+
+                System.out.println("Rating:");
+                double rating = input.nextFloat();
+
+                Book myBook;
+
+            if(trope.equals("")){
                 myBook = new Fiction(title, author, type, trope, numPages);
             }
             else{
                 myBook = new Book(title, author, type, numPages);
             }
 
-            //Book myBook = new Book(title, author, type, numPages);
-            //tbr.add(myBook);
 
-            FileWriter fWriter = new FileWriter("C:\\Users\\Ameya\\IdeaProjects\\LibraryProject\\tbr.txt", true);
-            fWriter.write(String.valueOf(myBook) + "\n");
-            fWriter.close();
 
-        }
-        else if(userAction.equals("modifyRead")){
-            System.out.println("Title:");
-            String title = input.nextLine();
+                myBook = new Fiction(title, author, type, trope, numPages, rating);
 
-            System.out.println("Type:");
-            String type = input.nextLine();
+                getReadRatingsAndPos(myBook, read);
+            } else if (userAction.equals("accessTBR")) {
+                File file = new File("tbr.txt");
+                Scanner fileReader = new Scanner(file);
 
-            System.out.println("Trope:");
-            String trope = input.nextLine();
+                while (fileReader.hasNextLine()) {
+                    System.out.println(fileReader.nextLine());
+                }
+            } else if (userAction.equals("accessRead")) {
+                File file = new File("read.txt");
+                Scanner fileReader = new Scanner(file);
 
-            System.out.println("Author:");
-            String author = input.nextLine();
-
-            System.out.println("Pages:");
-            int numPages = input.nextInt();
-
-            System.out.println("Rating:");
-            double rating = input.nextFloat();
-
-            Book myBook;
-/*
-            if(trope.length()>0){
-                myBook = new Fiction(title, author, type, trope, numPages);
-            }
-            else{
-                myBook = new Book(title, author, type, numPages);
-            }
-
- */
-
-            myBook = new Fiction(title, author, type, trope, numPages, rating);
-
-            getReadRatingsAndPos(myBook, read);
-        }
-        else if(userAction.equals("accessTBR")){
-            File file = new File("tbr.txt");
-            Scanner fileReader = new Scanner(file);
-
-            while (fileReader.hasNextLine()){
-                System.out.println(fileReader.nextLine());
-            }
-        }
-        else if(userAction.equals("accessRead")){
-            File file = new File("read.txt");
-            Scanner fileReader = new Scanner(file);
-
-            while (fileReader.hasNextLine()){
-                System.out.println(fileReader.nextLine());
+                while (fileReader.hasNextLine()) {
+                    System.out.println(fileReader.nextLine());
+                }
             }
         }
 
